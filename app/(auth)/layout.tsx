dffb2cb040c1d59e55cb9257ffa8576b7bfaae81
owenEEfobi <jenukohaefobi@gmail.com>
2024-11-22 01:47:51 -0700

    :chart_with_upwards_trend: added Umami analytics for user tracking, update layout.tsx and layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { getAuth } from "@/actions/auth";
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: {
@@ -21,8 +22,15 @@ export default async function AuthLayout({
 	}
 
 	return (
-		<div className="flex flex-col items-center justify-center h-screen">
-			{children}
-		</div>
+		<>
+			<Script
+				defer
+				src="https://cloud.umami.is/script.js"
+				data-website-id="fd1a8035-3637-4ae8-ae77-7cb317a6ac35"
+			/>
+			<div className="flex flex-col items-center justify-center h-screen">
+				{children}
+			</div>
+		</>
 	);
 }
--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -10,10 +10,10 @@ export async function generateMetadata(): Promise<Metadata> {
 	const store = await getStore();
 	return {
 		title: {
-			template: `%s | ${store?.name}`,
-			default: `${store?.name}`,
+			template: `%s | ${store.name}`,
+			default: `${store.name}`,
 		},
-		description: store?.maintenance
+		description: store.maintenance
 			? "The store is currently under maintenance"
 			: `Welcome to ${store?.name}`,
 	};
@@ -23,25 +23,34 @@ export default async function StoreLayout({
 	children,
 }: { children: ReactNode }) {
 	const store = await getStore();
-	return store.maintenance === false ? (
+	return (
 		<>
-			<StoreNav store={store} />
-			<main className="flex flex-col gap-4 p-4">{children}</main>
+			<Script
+				defer
+				src="https://cloud.umami.is/script.js"
+				data-website-id="fd1a8035-3637-4ae8-ae77-7cb317a6ac35"
+			/>
+			{store.maintenance === false ? (
+				<>
+					<StoreNav store={store} />
+					<main className="flex flex-col gap-4 p-4">{children}</main>
+				</>
+			) : (
+				<div className="flex h-screen items-center justify-center">
+					<div className="text-center p-8 bg-secondary rounded-lg shadow-lg">
+						<h1 className="text-3xl font-bold text-muted mb-4">
+							Store Maintenance
+						</h1>
+						<p className="text-xl mb-6">
+							We're currently updating our store to serve you better.
+						</p>
+						<div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 mx-auto" />
+						<p className="mt-6 ">
+							Please check back soon. We appreciate your patience!
+						</p>
+					</div>
+				</div>
+			)}
 		</>
-	) : (
-		<div className="flex h-screen items-center justify-center">
-			<div className="text-center p-8 bg-secondary rounded-lg shadow-lg">
-				<h1 className="text-3xl font-bold text-muted mb-4">
-					Store Maintenance
-				</h1>
-				<p className="text-xl mb-6">
-					We're currently updating our store to serve you better.
-				</p>
-				<div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 mx-auto" />
-				<p className="mt-6 ">
-					Please check back soon. We appreciate your patience!
-				</p>
-			</div>
-		</div>
 	);
 }
